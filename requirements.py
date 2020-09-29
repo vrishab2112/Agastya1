07abe72b25a75e3c70737f4875b7e8b1100cb388
vrishab2112 <68913832+vrishab2112@users.noreply.github.com>
2020-09-29 19:44:28 +0000

    Create requirements.py
    new file

---
+++ requirements.py
@@ -0,0 +1,12 @@
+import os,sys
+
+    os.system('apt-get install python-pip')
+    os.system('easy_install pip')
+    import pip   
+    
+    os.system('pip install speech_recognition')
+    os.system('pip install pyttsx3')
+    os.system('pip install datetime')
+    os.system('pip install wikipedia')
+    os.system('pip install webbrowser')
+    os.system('pip install os')
