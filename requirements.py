ee715d0ef0a9d85055b0d77355972c9da209ba02
vrishab2112 <68913832+vrishab2112@users.noreply.github.com>
2020-10-16 23:43:32 +0530

    Update requirements.py

--- requirements.py
+++ requirements.py
@@ -1,12 +1,7 @@
-import os,sys
-
-    os.system('apt-get install python-pip')
-    os.system('easy_install pip')
-    import pip   
-    
-    os.system('pip install speech_recognition')
-    os.system('pip install pyttsx3')
-    os.system('pip install datetime')
-    os.system('pip install wikipedia')
-    os.system('pip install webbrowser')
-    os.system('pip install os')
+pip install pip
+speech_recognition
+pyttsx3
+pip install datetime
+pip install wikipedia
+pip install webbrowser
+pip install os
